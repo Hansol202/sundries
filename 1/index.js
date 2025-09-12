7539977250c66f23147e63d9df275927054e0ab6
Hansol202 <gksthfeld@gmail.com>
2025-09-13 01:33:44 +0900

    Add files via upload

---
+++ 1/index.js
@@ -0,0 +1,39 @@
+let target = document.querySelector("#dynamic");
+let stringArr = ["Learn to HTML", "Learn to CSS", 
+    "Learn to JavaScript", "Learn to React", "Learn to Ruby"];
+let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+let selectStringArr = selectString.split("");
+
+function randomString(){
+    let target = document.querySelector("#dynamic");
+    let stringArr = ["Learn to HTML", "Learn to CSS", 
+    "Learn to JavaScript", "Learn to React", "Learn to Ruby"];
+    let selectString = stringArr[Math.floor(Math.random() * stringArr.length)];
+    let selectStringArr = selectString.split("");
+
+    return selectStringArr;
+
+}
+
+function resetTyping(){
+    target.textContent = "";
+    dynamic(randomString());
+}
+
+function dynamic(randomArr){
+    if(randomArr.length > 0){
+        target.textContent += randomArr.shift();
+        setTimeout(function(){
+            dynamic(randomArr);
+        }, 80);
+    }else{
+        setTimeout(resetTyping, 3000);
+    }
+}
+
+dynamic(randomString());
+
+function blink(){
+target.classList.toggle("active");
+}
+setInterval(blink, 500);
